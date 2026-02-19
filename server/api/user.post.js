c479b19b396395d8dd8d9a45c11e5c8a164451cb
harsh-096 <hpparmar8899@gmail.com>
2026-02-19 19:59:49 +0530

    Updated Validor on email and password

--- server/api/user.post.js
+++ server/api/user.post.js
@@ -5,6 +5,39 @@ export default defineEventHandler(async (event) => {
   const body = await readBody(event)
   const prisma = getPrismaClient()
 
+  const email = body.email?.trim()
+const password = body.password
+
+if (!email || !password) {
+  throw createError({
+    statusCode: 400,
+    message: "Email and password are required."
+  })
+}
+
+// Simple email validation
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+if (!emailRegex.test(email)) {
+  throw createError({
+    statusCode: 400,
+    message: "Invalid email format."
+  })
+}
+
+// Password: min 8 chars, 1 uppercase, 1 lowercase, 1 number, 1 special char
+// const passwordRegex =
+//   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_\-+=<>?/{}~]).{8,}$/
+
+if (password.length<=6) {
+  throw createError({
+    statusCode: 400,  
+    message:
+      "Password must be 6+ characters character."
+  })
+}
+
+
   const salt = await bcrypt.genSalt(10)
   const passwordHash = await bcrypt.hash(body.password, salt)
 
@@ -17,12 +50,33 @@ export default defineEventHandler(async (event) => {
       },
     })
 
-    return { success: true, user }
+    return {
+      success: true,
+      message: "Login successful",
+      user: {
+        id: user.id,
+        email: user.email
+      }
+     }
+
   } catch (error) {
-    console.error('Registration error:', error)
+
+    // Handle duplicate email error
+    if (error.code === "P2002") {
+      throw createError({
+        statusCode: 409,
+        message: "This email is already registered."
+      })
+    }
+
+    // Fallback for unexpected errors
+    console.error("Registration error:", error)
+
     throw createError({
-      statusCode: 400,
-      statusMessage: error.message,
+      statusCode: 500,
+      message: "Something went wrong during registration."
     })
   }
+
+
 })
